13ea9719bf8bd37cca724f8557efd1a51c7274d8
fov42550564 <42550564@qq.com>
2016-09-28 19:37:56 +0800

    完成使用android调用子app

--- project/android/app/src/main/java/com/remobile/utils/UtilsModule.java
+++ project/android/app/src/main/java/com/remobile/utils/UtilsModule.java
@@ -1,27 +1,30 @@
 package com.remobile.utils;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.os.PowerManager;
-
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.remobile.cordova.CordovaPlugin;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class UtilsModule extends ReactContextBaseJavaModule {
+public class UtilsModule extends CordovaPlugin {
 
     public static String FLOG_TAG = "UtilsModule";
 
 
     private Activity activity;
     private PowerManager.WakeLock wakeLock;
+    private Callback callback;
 
 
     public UtilsModule(ReactApplicationContext reactContext, Activity activity) {
-        super(reactContext);
+        super(reactContext, true);
         this.activity = activity;
     }
 
@@ -63,6 +66,25 @@ public class UtilsModule extends ReactContextBaseJavaModule {
         }
     }
 
+    @ReactMethod
+    public void startChildApp(String pkg, String cls, String param, Callback callback) {
+        try {
+            ComponentName componet = new ComponentName(pkg, cls);
+            Intent intent = new Intent();
+            intent.setComponent(componet);
+            intent.setAction("android.intent.action.MAIN");
+            intent.putExtra("param", param);
+            cordova.startActivityForResult((CordovaPlugin)this, intent, 0);
+            this.callback = callback;
+        } catch (Exception e) {
+            callback.invoke(true);
+        }
+    }
 
-
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            callback.invoke(false);
+        }
+    }
 }
