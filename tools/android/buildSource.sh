f066dd15fabdf784c0f4147f0a5df90d00caa655
fov42550564 <42550564@qq.com>
2017-04-12 13:49:38 +0800

    修正android编译源码的脚本

--- tools/android/buildSource.sh
+++ tools/android/buildSource.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-if [ "$1" = "clear" ];then
+if [ "$1" = "-h" -o "$1" = "--help" ];then
+    echo "buildSource.sh [-h|--help|-c|--clear|-r|--run]"
+    echo
+    echo "di ../../project/android/build.gradle ./source/build.gradle"
+    echo "di ../../project/android/settings.gradle ./source/settings.gradle"
+    echo "di ../../project/android/app/build.gradle ./source/app/build.gradle"
+    echo
+elif [ "$1" = "-c" -o "$1" = "--clear" ];then
     git co head ../../project/android/build.gradle
     git co head ../../project/android/settings.gradle
     git co head ../../project/android/app/build.gradle
-else
+elif [ "$1" = "-r" -o "$1" = "--run" ];then
     cp ./source/build.gradle ../../project/android/build.gradle
     cp ./source/settings.gradle ../../project/android/settings.gradle
     cp ./source/app/build.gradle ../../project/android/app/build.gradle
